3e9fef32fc27979bc03042387b6e28ad80c5ab2e
ahmaddhoh <116354855+ahmaddhoh@users.noreply.github.com>
2023-07-30 14:07:22 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# sos
+# Telegram Bot for University Marks Scraping
+
+[![Made with Python](https://img.shields.io/badge/Made%20with-Python-blue.svg)](https://img.shields.io/badge/Made%20with-Python-blue.svg)
+[![Open Source Love](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](https://github.com/ellerbrock/open-source-badges/)
+[![MIT License](https://img.shields.io/github/license/mashape/apistatus.svg)](https://github.com/sagarlad04/telegram-university-marks-bot/blob/master/LICENSE)
+
+This is a Telegram bot written in Python that generates an HTML file or a text message for a student's marks based on their ID number in [Albaath-Universty - IT College](https://exam.albaath-univ.edu.sy/exam-it/).
+
+## Table of Contents
+
+- [Telegram Bot for University Marks Scraping](#telegram-bot-for-university-marks-scraping)
+  - [Table of Contents](#table-of-contents)
+  - [About the Code](#about-the-code)
+  - [How the bot works](#how-the-bot-works)
+  - [Features](#features)
+  - [Prerequisites](#prerequisites)
+  - [Getting Started](#getting-started)
+  - [Contributions](#contributions)
+  - [License](#license)
+
+## About the Code
+
+This project is a modular bot, made using Python 3 and the following libraries:
+
+- [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
+- [aiohttp](https://docs.aiohttp.org/en/stable/)
+- [lxml](https://lxml.de/)
+
+## How the bot works
+
+The bot scrapes a student marks table and converts the list of students' marks into an HTML table. For example:
+
+<img
+  src="media/university_page_example.png"
+  alt="university_page_example"
+  width="400"/>
+
+The list of students' marks will look like this in HTML:
+
+<img
+  src="media/html_table.png"
+  alt="html_table"
+  width="400"/>
+
+## Features
+
+- The bot allows the user to obtain an HTML file of a single student's marks by providing their ID number using the `/html` command.
+- The bot also sends a normal text message of a student's marks to the user.
+- The bot allows the user to obtain HTML files of all students in a particular year using the `/year1`, `/year2`, `/year3`, `/year4`, and `/year5` commands.
+- The bot can also generate an HTML file of all students whose ID numbers are within a certain range using the `/in_range` command.
+- The bot can work in inline mode, so you can type your bot username then the ID number of a students _(you should enable inline mode in your bot from [@BotFather](https://telegram.me/botfather) settings > Inline Mode > Turn on)_.
+
+## Prerequisites
+
+Before you use this bot, you need to have:
+
+- A working Telegram account.
+- A bot token, which can be obtained by talking to [BotFather](https://telegram.me/botfather) on Telegram.
+
+## Getting Started
+
+1. Clone this repository to your local machine.
+
+    ```shell
+    git clone https://github.com/ObaidaNa/albaath-marks.git
+    cd albaath-marks
+    ```
+
+2. Add your bot token to the `config.json` file.
+3. Install the required packages using the following command:
+
+    ```shell
+    pip install -r requirements.txt
+    ```
+
+4. Run the bot:
+
+    ```shell
+    python source/main.py
+    ```
+
+> **Notes:**
+>
+> - You can change the start message in the `config.json` file
+> - Also you can specify the html file caption (when it sended to the user) and the HTML sign (which appears on the top of the HTML file)
+> - To convert an HTML file to PDF, here's some websites I've tested and was working fine with Arabic:
+>   - [pdfcrowd](https://pdfcrowd.com/html-to-pdf/#convert_by_upload)
+>   - [web2pdfconvert](https://www.web2pdfconvert.com/html/to/pdf/)
+## Contributions
+
+If you would like to contribute to this project, feel free to submit a pull request. All contributions are welcome and appreciated!
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ main.py
@@ -0,0 +1,199 @@
+import json
+import logging
+import asyncio
+import os
+import aiohttp
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Application, filters, MessageHandler, CommandHandler, ContextTypes, InlineQueryHandler
+from html_parser import html_maker, parse_to_text
+from uuid import uuid4
+from random import random
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def validate_input(numbers) -> bool:
+    for number in numbers:
+        if number.isdigit() and 1000 <= int(number) <= 6000:
+            return True
+    return False
+
+
+async def not_valid_input(update: Update):
+    await update.message.reply_text("الرقم الجامعي خاطئ .. أدخل رقما صحيحا ")
+
+
+async def inline_query(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.inline_query.query.split()[0]
+    if not validate_input([query]):
+        return
+    output = await inline_responser(update, context, query)
+    results = [
+        InlineQueryResultArticle(
+            id=str(uuid4()),
+            title="إظهار النتيجة",
+            description="يسمح بإدخال رقم جامعي واحد فقط",
+            input_message_content=InputTextMessageContent(
+                message_text=output, parse_mode="MarkdownV2")
+        )
+    ]
+    await update.inline_query.answer(results)
+
+
+async def inline_responser(update, context, number):
+    async with aiohttp.ClientSession() as session:
+        response = await one_req(number, session)
+    output = parse_to_text(response['html'], response['number'])
+    return output if output else "الرقم الامتحاني خاطئ"
+
+
+async def responser(update: Update, context: ContextTypes.DEFAULT_TYPE, numbers=(), html_bl=False):
+    if not numbers:
+        numbers = context.args if context.args else update.message.text.split()
+        isvalid = validate_input(numbers)
+        if not isvalid:
+            return await not_valid_input(update)
+
+    numbers.sort()
+    message = await update.message.reply_text("يتم الآن إحضار النتيجة من الموقع ، انتظر قليلا 📥")
+    try:
+        async with aiohttp.ClientSession() as session:
+            tasks = [asyncio.create_task(one_req(number, session))
+                     for number in numbers]
+            gathered = await asyncio.gather(*tasks)
+        if len(numbers) <= 5 and not html_bl:
+            await send_txt_results(update, gathered)
+        else:
+            await message.edit_text("⌛️ يتم التحويل إلى ملف html ...")
+            html_filename = html_maker(gathered)
+            filename = "marks_" + str(int(random() * 100000)) + '.html'
+            with open('config.json', 'r') as f:
+                caption = json.load(f).get('caption')
+            await update.message.reply_document(html_filename, caption=caption, filename=filename)
+
+    except Exception:
+        await update.message.reply_text("هنالك مشكلة ما في الموقع ، أعد المحاولة لاحقا ", quote=True)
+        for task in tasks:
+            task.cancel()
+    finally:
+        await message.delete()
+
+
+async def in_range(update: Update, context: ContextTypes.DEFAULT_TYPE, args=()):
+    if not args:
+        args = context.args
+    strt, end = int(args[0]), int(args[1])
+    if strt > end:
+        strt, end = end, strt
+    if strt < 1000 or end > 6000:
+        await update.message.reply_text("أدخل مجال صحيح")
+        return
+    await responser(update, context, [x for x in range(strt, end)])
+
+
+async def one_req(number, session: aiohttp.ClientSession, recurse=0) -> dict[bytes, int]:
+    if recurse > 10:
+        raise Exception("uncompleted request, try again later")
+
+    url = "https://exam.albaath-univ.edu.sy/exam-civil/"
+    async with session.post(url, data={'number1': number}) as req:
+        res_data = await req.read()
+    if req.status != 200:
+        return await one_req(number, session, recurse+1)
+    return {'html': res_data, 'number': number}
+
+
+async def send_txt_results(update: Update, students):
+    for student in students:
+        output = parse_to_text(student['html'], student['number'])
+        if not output:
+            await update.message.reply_text(f"الرقم الامتحاني {student['number']} خاطئ", quote=True)
+        else:
+            await update.message.reply_text(output, parse_mode="MarkdownV2")
+
+
+# some redirecting functions
+async def html_it(*args):
+    await responser(*args, html_bl=True)
+
+
+async def year1(*args):
+    return await in_range(*args, args=(1000, 2000))
+
+
+async def year2(*args):
+    return await in_range(*args, args=(2000, 3000))
+
+
+async def year3(*args):
+    return await in_range(*args, args=(3000, 4000))
+
+
+async def year4(*args):
+    return await in_range(*args, args=(4000, 5000))
+
+
+async def year5(*args):
+    return await in_range(*args, args=(5000, 6000))
+
+
+async def start(update: Update, context) -> None:
+
+    with open('config.json', 'r', encoding='utf-8') as f:
+        output = json.load(f)['start']
+    await update.message.reply_text(output, parse_mode="MarkdownV2",
+                                    disable_web_page_preview=True)
+
+
+def get_token() -> str:
+    filename = "config.json"
+    if not os.path.exists(filename):
+        init_config_file()
+
+    with open(filename, "r") as file:
+        config = json.load(file)
+    token = config["BOT_TOKEN"]
+    if token == "6422430655:AAEIjACABKcHovv6-IyGmPr9dYVzP9AyYrU":
+        raise Exception(
+            "Please add your bot token, get it from https://t.me/botfather")
+    return token
+
+
+def init_config_file():
+    token = input("Please input your bot token (get it from @Botfather):\n")
+    with open('config.json', 'w') as f:
+        json.dump({'BOT_TOKEN': token, 'start': 'Hello'}, f)
+
+
+def main() -> None:
+    token = get_token()
+    application = Application.builder().token(token).build()
+    application.add_handlers(
+        [
+            CommandHandler(["start", "help"], start),
+            CommandHandler("in_range", in_range),
+            CommandHandler("html", html_it),
+            CommandHandler('year1', year1),
+            CommandHandler('year2', year2),
+            CommandHandler('year3', year3),
+            CommandHandler('year4', year4),
+            CommandHandler('year5', year5),
+            InlineQueryHandler(inline_query),
+            MessageHandler(
+                filters.TEXT & ~filters.COMMAND, callback=responser)
+        ]
+    )
+    application.add_error_handler(error)
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
